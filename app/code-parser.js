dc912c9913caca027274b9939fbd5c97aaf508e2
Ryan Bennett <nomad.ry@gmail.com>
2023-10-31 00:13:06 -0600

    custom node

---
+++ app/code-parser.js
@@ -0,0 +1,54 @@
+import BespeakComponent from "./component.js";
+import { html, css } from "https://esm.sh/lit";
+import { getMarkdownCodeBlocks } from "./util.js";
+
+export default class CodeParser extends BespeakComponent {
+    static styles = css`
+        :host {
+            display: flex;
+            flex-direction: column;
+        }
+        ::slotted(*) {
+            width: 100%;
+        }
+        button {
+            margin-top: 1rem;
+        }
+    `;
+
+    static config = {
+        type: "object",
+        properties: {
+            language: {
+                type: "string",
+                enum: ["javascript", "json"],
+                default: "javascript",
+            },
+        },
+    };
+
+    static output = {
+        title: "code",
+        type: "string",
+    };
+
+    icon = "code";
+
+    async _process(input, config, keys) {
+        const gptInput = input.find((schema) => schema.schema.title === "GPT");
+        if (!gptInput) {
+            return this.output;
+        }
+
+        const codeBlocks = getMarkdownCodeBlocks(gptInput.value.response);
+        const matchingBlocks = codeBlocks.filter(
+            (block) => block.language === config.language
+        );
+
+        if (matchingBlocks.length === 0) {
+            return "";
+        }
+
+        return matchingBlocks.pop().code;
+    }
+}
